602f29ad135f397fc4edcd86737453f220dbcdee
niftylettuce <niftylettuce@gmail.com>
2018-05-21 04:01:50 -0500

    Added CNAME

---
+++ config.js
@@ -0,0 +1,15 @@
+docute.init({
+  debug: true,
+  title: 'Lipo.io',
+  repo: 'lipojs/lipo',
+  'edit-link': 'https://github.com/lipojs/lipo/tree/master/',
+  twitter: 'niftylettuce',
+  nav: {
+    default: [
+      {
+        title: 'Free image manipulation API service built on top of Sharp',
+        path: '/'
+      }
+    ]
+  }
+});
